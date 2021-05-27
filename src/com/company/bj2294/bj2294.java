dae93c0f50b1efa55b0783f259e78c38c8173272
sky6min <sky6min@gmail.com>
2021-05-27 15:30:39 +0900

    setting1

---
+++ src/com/company/bj2294/bj2294.java
@@ -0,0 +1,8 @@
+package com.company.bj2294;
+
+public class bj2294 {
+
+    public void Solution() {
+        System.out.println("hello java");
+    }
+}
---
+++ src/com/company/main/Main.java
@@ -0,0 +1,12 @@
+package com.company.main;
+
+import com.company.bj2294.bj2294;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        bj2294 solution = new bj2294();
+        solution.Solution();
+    }
+}
